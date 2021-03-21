ca6e25db5dc24a01bc73412d900fcfeacd1f2a1d
Aleksey Ulyanov <raccoondrinkgreentea@gmail.com>
2021-03-21 22:17:00 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+async function converter(array) {
+	return new Promise((res, rej) => {
+		let result = '';
+		if (array.length) {
+			result = array.reduce((acc, curr, i, arr) => {
+				// Собираем массив массивов с последовательно идущими значениями
+				(i !== 0 && curr - arr[i-1] === 1) ? acc[acc.length-1].push(curr) : acc.push([curr]);
+				return acc;
+			}, []).map(arr => {
+				// Определяем через что соединить значения
+				return arr.length > 2 ? `${arr[0]}-${arr.slice(-1)}` : `${arr.join(',')}`;
+			}).join(',');
+		}
+		res(result);
+	});
+};
+
+export { converter };
---
+++ test/test.spec.js
@@ -0,0 +1,95 @@
+import { expect } from "chai";
+import { converter } from "../index.js";
+
+describe("converter", () => {
+	it("should convert [1,2,3,4,5,6,7,8] to '1-8'", done => {
+		converter([1, 2, 3, 4, 5, 6, 7, 8]).then((result) => {
+			expect(result).to.equal('1-8');
+			done();
+		});
+	});
+
+	it("should convert [1,3,4,5,6,7,8] to '1,3-8'", done => {
+		converter([1, 3, 4, 5, 6, 7, 8]).then((result) => {
+			expect(result).to.equal('1,3-8');
+			done();
+		});
+	});
+
+	it("should convert [1,3,4,5,6,7,8,10,11,12] to '1,3-8,10-12'", done => {
+		converter([1, 3, 4, 5, 6, 7, 8, 10, 11, 12]).then((result) => {
+			expect(result).to.equal('1,3-8,10-12');
+			done();
+		});
+	});
+
+	it("should convert [1,2,3] to '1-3'", done => {
+		converter([1, 2, 3]).then((result) => {
+			expect(result).to.equal('1-3');
+			done();
+		});
+	});
+
+	it("should convert [1,2] to '1,2'", done => {
+		converter([1, 2]).then((result) => {
+			expect(result).to.equal('1,2');
+			done();
+		});
+	});
+
+	it("should convert [1,2,4] to '1,2,4'", done => {
+		converter([1, 2, 4]).then((result) => {
+			expect(result).to.equal('1,2,4');
+			done();
+		});
+	});
+
+	it("should convert [1,2,4,5,6] to '1,2,4-6'", done => {
+		converter([1, 2, 4, 5, 6]).then(result => {
+			expect(result).to.equal('1,2,4-6');
+			done();
+		});
+	});
+
+	it("should convert [1,2,3,7,8,9,15,17,19,20,21] to '1-3,7-9,15,17,19-21'", done => {
+		converter([1, 2, 3, 7, 8, 9, 15, 17, 19, 20, 21]).then(result => {
+			expect(result).to.equal('1-3,7-9,15,17,19-21');
+			done();
+		});
+	});
+
+	it("should convert [1,2,3,4,5,6,100,1091,1999,2000,2001,2002] to '1-6,100,1091,1999-2002'", done => {
+		converter([1, 2, 3, 4, 5, 6, 100, 1091, 1999, 2000, 2001, 2002]).then(result => {
+			expect(result).to.equal('1-6,100,1091,1999-2002');
+			done();
+		});
+	});
+
+	it("should convert [1] to '1'", done => {
+		converter([1]).then(result => {
+			expect(result).to.equal('1');
+			done();
+		});
+	});
+
+	it("should convert [1,3,5,7,9,11] to '1,3,5,7,9,11'", done => {
+		converter([1, 3, 5, 7, 9, 11]).then(result => {
+			expect(result).to.equal('1,3,5,7,9,11');
+			done();
+		});
+	});
+
+	it("should convert [1,2,3,5,6] to '1-3,5,6'", done => {
+		converter([1, 2, 3, 5, 6]).then(result => {
+			expect(result).to.equal('1-3,5,6');
+			done();
+		});
+	});
+
+	it("should convert [] to ''", done => {
+		converter([]).then(result => {
+			expect(result).to.equal('');
+			done();
+		});
+	});
+});
